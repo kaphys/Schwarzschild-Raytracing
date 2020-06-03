10ebb39df6dbbaf59a698990c12cfa5569311a3d
Alex <a.m.debievre@student.tudelft.nl>
2020-06-03 14:35:05 +0000

    Ray trace code without gravitational lensing.

---
+++ raytrace.py
@@ -0,0 +1,76 @@
+import numpy as np
+import math as m
+from PIL import Image
+
+# Window dimensions
+W = 200
+H = 200
+# Camera settings
+S_RATIO = 1 / 50
+Z_START = 4
+OBSERVER = [0, 0, 2]
+
+# Schwarzschild radius
+M = 0.5
+ORIGIN = [0, 0, 5]
+BG = 5
+# Iterations
+STEPS = 100
+SIZE = 0.1
+
+# Blackhole color
+BLACK = [0, 0, 0]
+
+data = np.zeros((H, W, 3), dtype=np.uint8)
+ray = np.zeros((H, W, 3))
+
+
+def colorangles(ray):
+    """
+    angle at origin;
+    polar angle of ray: phi
+    azimuthal angle of ray: theta
+
+    gives back color for background:
+    depending on angle; theta and phi
+    only after norm exceeds certain value.
+    0 =< theta < 2pi
+    0 =< phi =< pi
+    """
+    GREY = ([150, 150, 150])
+    WHITE = ([255, 255, 255])
+
+    norm = np.linalg.norm(ray)
+    xn = ray[0]/norm
+    yn = ray[1]/norm
+    zn = ray[2]/norm
+    phi = np.arccos(yn)
+    theta = m.atan2(zn, xn)
+
+    switch = (int(20*theta/np.pi) + int(20*phi/np.pi))% 2 == 0
+    if switch == 0:
+        bg_color = GREY
+    else:
+        bg_color = WHITE
+
+    return bg_color
+
+
+for i in range(H):
+    for j in range(W):
+        ray[i, j, :] = np.array([(i - 1 / 2 * H) * S_RATIO, (j - 1 / 2 * W) * S_RATIO, Z_START])
+        p = ray[i, j, :] - OBSERVER
+        direction = p/np.linalg.norm(p)
+        for a in range(STEPS):
+            if np.linalg.norm((ray[i, j, :] - ORIGIN)) <= M:
+                data[i, j, :] = BLACK
+                break
+            elif np.linalg.norm((ray[i, j, :] - ORIGIN)) >= BG:
+                data[i, j, :] = colorangles(ray[i, j, :] - ORIGIN)
+                break
+            else:
+                ray[i, j, :] += SIZE * direction
+
+im = Image.fromarray(data)
+im.show()
+im = im.save("linear_render.png")
